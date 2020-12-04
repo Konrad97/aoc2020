9b3647bc12028b56ff0cccc5cdd6d08fa3514740
Konrad <melzerkonrad1@gmail.com>
2020-12-04 16:42:23 +0100

    Add dropLast... ty @cdi

--- src/main/kotlin/four/b/main.kt
+++ src/main/kotlin/four/b/main.kt
@@ -9,12 +9,15 @@ val requiredFields = mapOf<String, (String) -> Boolean>(
     "iyr" to { it.length == 4 && it.toInt() in 2010..2020 },
     "eyr" to { it.length == 4 && it.toInt() in 2020..2030 },
     "hgt" to {
-        it.split("c", "i").run {
-            if (last() == "m") first().toInt() in 150..193 else first().toInt() in 59..76
+        when {
+            it.endsWith("cm") -> it.dropLast(2).toInt() in 150..193
+            it.endsWith("in") -> it.dropLast(2)
+                .toInt() in 59..76
+            else -> false
         }
     },
-    "hcl" to { it.startsWith('#') && it.substring(1).all { char -> char in "0123456789abcdef"} },
-    "ecl" to { it in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")},
+    "hcl" to { it.startsWith('#') && it.substring(1).all { char -> char in "0123456789abcdef" } },
+    "ecl" to { it in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth") },
     "pid" to { it.length == 9 && it.all { digit -> digit in "0123456789" } },
 )
 
